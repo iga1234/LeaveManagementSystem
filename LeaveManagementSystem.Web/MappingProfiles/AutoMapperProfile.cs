b0d102ee3f40521a49529350309482f8abc2cd3f
icieszkowska <iga.cieszkowska@intel.com>
2024-10-29 13:44:58 +0100

    Add Service Layer

---
+++ LeaveManagementSystem.Web/MappingProfiles/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LeaveManagementSystem.Web.Data;
+using LeaveManagementSystem.Web.Models.LeaveTypes;
+
+namespace LeaveManagementSystem.Web.MappingProfiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile() {
+            CreateMap<LeaveType, LeaveTypeReadOnlyVM>();
+            //When in IndexVM variable name is Days and in LeaveType is NumberOfDays 
+            //.ForMember(dest => dest.NumberOfDays, opt => opt.MapFrom(src => src.NumberOfDays));
+            CreateMap<LeaveTypeCreateVM, LeaveType>();
+            CreateMap<LeaveTypeEditVM, LeaveType>().ReverseMap();
+        }
+
+    }
+}
